205cfa74418fa0f4838c31377e902a66547568c5
Liu Ziqian <gregliuc@gmail.com>
2024-11-10 10:20:20 +0800

    Upload the data

--- S_phi_plot_multi.py
+++ S_phi_plot_multi.py
@@ -8,6 +8,7 @@ import random
 import matplotlib.pyplot as plt
 import math
 import os
+import pandas as pd
 
 
 # Load one month's data
@@ -18,6 +19,12 @@ def load_monthly_network(month_to_load):
 
     return loaded_graph
 
+def site_percolation(graph, p):
+    H = graph.copy()
+    for node in list(H.nodes()):
+        if random.random() < p:
+            H.remove_node(node)
+    return H
 
 # Percolation functions
 def percolation_single_iteration(graph, removal_fraction):
@@ -29,14 +36,9 @@ def percolation_single_iteration(graph, removal_fraction):
     initial_largest_component = max(initial_components, key=len)
     initial_size = len(initial_largest_component)
 
-    # Calculate the number of nodes to remove
-    num_nodes_to_remove = int(len(percolated_graph.nodes()) * removal_fraction)
+    # Remove nodes
+    percolated_graph = site_percolation(percolated_graph, removal_fraction)
 
-    # Randomly select nodes to remove
-    nodes_to_remove = random.sample(list(percolated_graph.nodes()), num_nodes_to_remove)
-
-    # Remove the selected nodes
-    percolated_graph.remove_nodes_from(nodes_to_remove)
 
     # Calculate the final component size
     final_components = list(nx.connected_components(percolated_graph))
@@ -70,8 +72,9 @@ def percolation(graph, removal_fraction=0.1, num_iterations=100):
 
 
 # Plot percolation curve
-def plot_percolation(graph, month_to_load, num_iterations=100, removed_range=(0.0, 1.0), step=0.1):
+def plot_percolation(graph, month_to_load, num_iterations=500, removed_range=(0.0, 1.0), step=0.01):
     total_nodes = nx.number_of_nodes(graph)
+    print(f"Month: {month_to_load}, Total nodes: {total_nodes}")
 
     # Create a list of fractions to remove
     removed_fraction = [round(x * step, 2) for x in range(int(removed_range[0]/step), int(removed_range[1]/step)+1)]
@@ -84,29 +87,34 @@ def plot_percolation(graph, month_to_load, num_iterations=100, removed_range=(0.
         print(f"Fraction: {fraction}, True size: {true_size}, Final size: {avg_final_size}")
         if true_size == 0:
             component_fraction.append(0)
+        elif true_size < avg_final_size:
+            component_fraction.append(0)
         else:
             component_fraction.append(avg_final_size / true_size)
         #component_fraction.append(avg_final_size / total_nodes)
     
     # Plot the results
     phi = removed_fraction[::-1]
+    plt.figure()
     plt.plot(phi, component_fraction)
     plt.xlabel(r"$\Phi$")
     plt.ylabel("Giant Cluster Size")
-    plt.title("Percolation Curve")
+    plt.title(f"Component Fraction vs. Phi for {month_to_load}")
 
     # Save the plot
-    plt.savefig(f"./percolation_figure/{month_to_load}.png")
+    plt.savefig(f"./S_Phi_figure/{month_to_load}.png")
 
     return
 
 
 # Main function
 def main():
-    month_to_load = "2001-05"
-    loaded_graph = load_monthly_network(month_to_load)
-    plot_percolation(loaded_graph, month_to_load)
-
+    # Generate date range from 1999-01 to 2001-07
+    date_range = pd.date_range(start='1999-01', end='2001-08', freq='ME')
+    formatted_dates = date_range.strftime('%Y-%m').tolist()
+    for month in formatted_dates:
+        loaded_graph = load_monthly_network(month)
+        plot_percolation(loaded_graph, month)
 
 if __name__ == "__main__":
     main()
